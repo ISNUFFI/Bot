6e067f51f9017adb111b51335227b1dfd1944943
GR <rozov2004@list.ru>
2020-05-28 22:59:11 +0300

    1231231

--- Bot.py
+++ Bot.py
@@ -3,19 +3,22 @@ import config
 bot = telebot.TeleBot(config.token)
 
 
+def generate_markup():
+    markup = bot.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup.add('Привет')
+    
+
 @bot.message_handler(commands=['start'])
 def st_message(message):
-    bot.send_message(message.chat.id, 'Hello World!')
-    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAP_Xs_rg-rzFzCJK46JaqHmwjcWjNIAAk0cAAIkhhQFBVasJEbr6z8ZBA')
+    generate_markup()
+    bot.send_message(message.chat.id, 'Hello!')
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     print(message.text, message.chat.first_name)
     bot.send_message(message.chat.id, message.text)
-    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIBjF7P7AOoo4vgRL2KlUKtbd7y4YwtAAJVAQAC81Y_F3TXHEt6LzP4GQQ')
-    if message.text.lower() == 'dice' :
-        bot.get_user_profile_photos(message.chat.id)
+
 
 
 @bot.message_handler(content_types=['sticker'])
