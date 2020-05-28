b78ce6ed83ccb589475afb8de5e06836f5cb077e
GR <rozov2004@list.ru>
2020-05-28 21:31:04 +0300

    123

--- Bot.py
+++ Bot.py
@@ -3,8 +3,6 @@ import config
 bot = telebot.TeleBot(config.token)
 
 
-Button1 = telebot.types.KeyboardButton('Throw the dice')
-telebot.types.ReplyKeyboardMarkup(Button1, True)
 
 @bot.message_handler(commands=['start'])
 def st_message(message):
@@ -16,9 +14,6 @@ def send_text(message):
     print(message.text, message.chat.first_name)
     bot.send_message(message.chat.id, message.text)
     bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIBjF7P7AOoo4vgRL2KlUKtbd7y4YwtAAJVAQAC81Y_F3TXHEt6LzP4GQQ')
-    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIBrl7P89Jj4gHDo1g1HvjkpjmQ5CpmAAIgAAONAAHOMadDuA1w4KC4GQQ')
-    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIBql7P83NJ-oYmQHd6yoQpM3ycGiezAAJVAQAC81Y_F3TXHEt6LzP4GQQ')
-    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIBqV7P82i5W7t1Q8w_yXzEqXdIwM5PAAKFAQACmZhGEZ4-vwN_gxrlGQQ')
 
 @bot.message_handler(content_types=['sticker'])
 def send_stickid(message):
