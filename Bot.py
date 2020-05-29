d977f6bea506447db53e2d5e0674d95e9294c98e
GR <rozov2004@list.ru>
2020-05-29 18:13:46 +0300

    dyr

--- Bot.py
+++ Bot.py
@@ -10,14 +10,18 @@ keyboard1 = types.ReplyKeyboardMarkup(one_time_keyboard=False, resize_keyboard=T
 keyboard1.row('Кинуть кости {}'.format(config.dice))
 
 
-@bot.message_handler(commands=['test'])
-def find_file_ids(message):
-    for file in os.listdir("/Users/SNUFF/Pictures/tg/"):
-        f = open("/Users/SNUFF/Pictures/tg/"+file, 'rb')
-        msg = bot.send_photo(message.chat.id, f, None)
-        # А теперь отправим вслед за файлом его file_id
-        bot.send_message(message.chat.id, msg.photo.file_id, reply_to_message_id=msg.message_id)
-    time.sleep(3)
+@bot.message_handler(commands=['start'])
+def st_message(message):
+    bot.send_message(message.chat.id, 'Hello!', reply_markup=keyboard1)
+    print(message.chat.first_name)
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    print(message.text, message.chat.first_name)
+    if message.text.lower() == 'кинуть кости {}'.format(config.dice):
+        rnd = randint(1, 6)
+        bot.send_message(message.chat.id, config.dices[rnd])
 
 
 bot.polling()
--- etst.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path = "/Users/SNUFF/Pictures/tg/"
-dir_list = os.listdir(path)
-
-print("Files and directories in '", path, "' :")
-
-# print the list
-print(dir_list)
