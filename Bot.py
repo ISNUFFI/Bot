a947ea8cc5fa21faf1623f1c9aa270c0d4e815b2
GR <rozov2004@list.ru>
2020-05-29 03:26:51 +0300

    jopa

--- Bot.py
+++ Bot.py
@@ -6,23 +6,6 @@ import os
 import time
 bot = telebot.TeleBot(config.token)
 
-keyboard1 = types.ReplyKeyboardMarkup(one_time_keyboard=False, resize_keyboard=True)
-keyboard1.row('Кинуть кости {}'.format(config.dice))
-
-
-@bot.message_handler(commands=['start'])
-def st_message(message):
-    bot.send_message(message.chat.id, 'Hello!', reply_markup=keyboard1)
-    print(message.chat.first_name)
-
-
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    print(message.text, message.chat.first_name)
-    if message.text.lower() == 'кинуть кости {}'.format(config.dice):
-        rnd = randint(1, 6)
-        bot.send_message(message.chat.id, config.dices[rnd])
-
 
 @bot.message_handler(commands=['test'])
 def find_file_ids(message):
