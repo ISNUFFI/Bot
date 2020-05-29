c2b0307aa6dcaa551bc8c90520f6e2eb58a20a8b
GR <rozov2004@list.ru>
2020-05-29 03:24:43 +0300

    213

--- Bot.py
+++ Bot.py
@@ -2,6 +2,8 @@ from telebot import types
 from random import randint
 import telebot
 import config
+import os
+import time
 bot = telebot.TeleBot(config.token)
 
 keyboard1 = types.ReplyKeyboardMarkup(one_time_keyboard=False, resize_keyboard=True)
@@ -20,6 +22,16 @@ def send_text(message):
     if message.text.lower() == 'кинуть кости {}'.format(config.dice):
         rnd = randint(1, 6)
         bot.send_message(message.chat.id, config.dices[rnd])
+
+
+@bot.message_handler(commands=['test'])
+def find_file_ids(message):
+    for file in os.listdir('C:/Users/SNUFF/Pictures/tg'):
+        f = open('C:/Users/SNUFF/Pictures/tg'+file, 'rb')
+        msg = bot.send_photo(message.chat.id, f, None)
+        # А теперь отправим вслед за файлом его file_id
+        bot.send_message(message.chat.id, msg.photo.file_id, reply_to_message_id=msg.message_id)
+    time.sleep(3)
 
 
 bot.polling()
