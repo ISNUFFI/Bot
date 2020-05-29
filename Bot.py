8945998a8b763bf2952fe958d2e6791e6345ec04
GR <rozov2004@list.ru>
2020-05-29 03:29:46 +0300

    123

--- Bot.py
+++ Bot.py
@@ -9,8 +9,8 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['test'])
 def find_file_ids(message):
-    for file in os.listdir('C:/Users/SNUFF/Pictures/tg'):
-        f = open('C:/Users/SNUFF/Pictures/tg'+file, 'rb')
+    for file in os.listdir('Pictures/tg'):
+        f = open('Pictures/tg'+file, 'rb')
         msg = bot.send_photo(message.chat.id, f, None)
         # А теперь отправим вслед за файлом его file_id
         bot.send_message(message.chat.id, msg.photo.file_id, reply_to_message_id=msg.message_id)
